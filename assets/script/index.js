a0bce9571edc5dfa3975422581e531b9bb106675
Noah Miller <Noseby@live.ca>
2023-03-10 12:06:56 -0600

    form changed

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,6 +5,8 @@
 
     Assignment 1
 
+    _Clock_
+
 */
 
 setInterval(displayTime, 1000);
@@ -26,3 +28,51 @@ function displayTime() {
 }
 
 displayTime();
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Application
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+// Store date object of alarm
+let alarm = null;
+// Store reference to set alarm and loop till clear
+let alarmTimeout = null;
+
+const alarmSound = new Audio('./assets/audio/alarm.wav');
+alarmSound.type = 'audio/wav';
+
+const clock = document.querySelector('.clock')
+const output = document.querySelector('.output')
+const input = document.querySelector('.input')
+const button = document.querySelector('.set')
+
+// function to display time in div
+
+function display() {
+  output.innerHTML = input.value;
+}
+button.addEventListener('click', display);
+
+// Set time to start alarm. must add onchange to html file
+function setAlarmTime(value) {
+  alarm = value;
+  // console.log(alarm)
+};
+
+// set onclick to button to assign setAlarm to it
+function setAlarm() {
+  if(alarm) {
+    // get current date again
+    let current = new Date();
+    // create another date object with alarm time
+    const alarmLength = new Date(alarm);
+
+    if (alarmLength > current) {
+      let timeout = alarmLength.getTime() - current.getTime();
+      alarmTimeout = setTimeout(() => alarmSound.play(), timeout)
+      alert('Alarm set')
+    }
+  }
+}

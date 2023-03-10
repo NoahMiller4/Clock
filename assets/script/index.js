d67c155040bd793ae8953d5c29d904f159795507
Noah Miller <Noseby@live.ca>
2023-03-10 12:32:34 -0600

    Final changes added

--- assets/script/index.js
+++ assets/script/index.js
@@ -40,18 +40,17 @@ let alarm = null;
 // Store reference to set alarm and loop till clear
 let alarmTimeout = null;
 
-const alarmSound = new Audio('./assets/audio/alarm.wav');
-alarmSound.type = 'audio/wav';
+const alarmSound = new Audio('./assets/audio/alarm.mp3');
+alarmSound.type = 'audio/mp3';
 
 const clock = document.querySelector('.clock')
 const output = document.querySelector('.output')
 const input = document.querySelector('.input')
 const button = document.querySelector('.set')
 
-// function to display time in div
-
+// function to display time in div, trim to last 5 characters
 function display() {
-  output.innerHTML = input.value;
+  output.innerHTML = input.value.slice(-5);
 }
 button.addEventListener('click', display);
 
@@ -62,6 +61,7 @@ function setAlarmTime(value) {
 };
 
 // set onclick to button to assign setAlarm to it
+// must use window setTimeout to apply a timer
 function setAlarm() {
   if(alarm) {
     // get current date again
@@ -72,7 +72,17 @@ function setAlarm() {
     if (alarmLength > current) {
       let timeout = alarmLength.getTime() - current.getTime();
       alarmTimeout = setTimeout(() => alarmSound.play(), timeout)
-      alert('Alarm set')
     }
   }
+};
+
+// function to clear the audio, clear all timeouts
+// don't forget to set onclick to clear button to access function
+function clearAll() {
+  alarmSound.pause();
+  if (alarmTimeout) {
+    clearTimeout(alarmTimeout);
+  }
+  output.innerHTML = '00:00';
+  button.addEventListener('click', display);
 }
